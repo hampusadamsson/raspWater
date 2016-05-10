362face7d79ed157701ba1614858cc7072294e7c
thevoncarl <viktor.palerius@gmail.com>
2016-05-10 11:25:37 +0200

    Added all get functions to plants

--- viktor/Plants.py
+++ viktor/Plants.py
@@ -1,5 +1,40 @@
-
+import Bluetooth as bt
+MOISTURE_1 = "0"
+TEMP = "1"
+HUMIDITY = "2"
+WATER_LEVEL = "3"
+ACTIVATE_PUMP = "4"
+MOISTURE_2 = "5"
 
 
 
 def getHumidity1(socket):
+  bt.sendMessage(socket,MOISTURE_1)
+  data = bt.recieveMessage(socket)
+  return data
+  
+def getHumidity2(socket):
+  bt.sendMessage(socket,MOISTURE_2)
+  data = bt.recieveMessage(socket)
+  return data
+  
+def getTemperature(socket)
+  bt.sendMessage(socket,TEMP)
+  data = bt.recieveMessage(socket)
+  return data
+
+def getHumidity(socket)
+  bt.sendMessage(socket,HUMIDITY)
+  data = bt.recieveMessage(socket)
+  return data
+  
+def getWaterLevel(socket)
+  bt.sendMessage(socket,WATER_LEVEL)
+  data = bt.recieveMessage(socket)
+  return data
+  
+def activePump(socket)
+  bt.sendMessage(socket,ACTIVATE_PUMP)
+  data = bt.recieveMessage(socket)
+  print data
+  
